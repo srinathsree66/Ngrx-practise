cefc564c08eea21035aec0bb4da8dbc377c16c3c
SrinathSree <113054616+srinathsree66@users.noreply.github.com>
2022-11-12 15:42:00 +0000

    created auth module

---
+++ ngrx-practise/src/app/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [RegisterComponent],
+  providers: [],
+})
+export class AuthModule {}
---
+++ ngrx-practise/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  template: './register.component.html',
+  styleUrls: [],
+})
+export class RegisterComponent {}
