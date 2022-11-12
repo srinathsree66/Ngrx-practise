e0b830b1906ccf94249d352839dad164d969be5d
SrinathSree <113054616+srinathsree66@users.noreply.github.com>
2022-11-12 17:13:40 +0000

    signup form created

--- ngrx-practise/src/app/auth/auth.module.ts
+++ ngrx-practise/src/app/auth/auth.module.ts
@@ -1,10 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 @NgModule({
-  imports: [CommonModule],
   declarations: [RegisterComponent],
+
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   providers: [],
 })
 export class AuthModule {}
--- ngrx-practise/src/app/auth/components/register/register.component.ts
+++ ngrx-practise/src/app/auth/components/register/register.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  template: './register.component.html',
-  styleUrls: [],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.intializeForm();
+  }
+  intializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+  onSubmit(): void {
+    console.log('submitn', this.form.value, this.form.valid);
+  }
+}
